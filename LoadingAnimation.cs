f84fb15c528552b6c4a069b358630d9b21b0d970
Ladislav Havlát <ladislav.havlat@gmail.com>
2011-07-11 22:06:43 +0200

    + LoadingAnimation control

---
+++ LoadingAnimation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LH.Controls
+{
+    public partial class LoadingAnimation : UserControl
+    {
+        public LoadingAnimation()
+        {
+            InitializeComponent();
+        }
+    }
+}
